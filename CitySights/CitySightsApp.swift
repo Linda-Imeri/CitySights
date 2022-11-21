d9902d5513a661bf15b2f17bc34cf00e21eefff2
Linda-Imeri <lindaimmeri@gmail.com>
2022-11-21 01:55:17 +0100

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Linda Imeri on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
